9a57de5e20956f5eba636e4a9cc8bede2a982f00
frankbility34 <frankedoh395@gmail.com>
2026-09-07 11:38:17 -0700

    Prepare Django settings for production

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
 # Render automatically provides RENDER_EXTERNAL_HOSTNAME.
