0ba28d2f33e41f7beec19dd2821c5d367a7220f8
frankbility34 <frankedoh395@gmail.com>
2026-09-07 12:37:29 -0700

    Add production error logging

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -280,3 +280,23 @@ if not DEBUG:
 # ============================================================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
