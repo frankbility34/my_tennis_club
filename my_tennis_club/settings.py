0672e8314b98d73fe0c17b297a693d45a2e9ecbf
frankbility34 <frankedoh395@gmail.com>
2026-09-09 10:51:00 -0700

    Fix Hostinger email settings

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -311,13 +311,9 @@ import os
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.hostinger.com"
-EMAIL_PORT = 465
-
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-
-EMAIL_TIMEOUT = 10
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.hostinger.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
